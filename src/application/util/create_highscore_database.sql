287f801acbc92864bcab0c28448eea5686bd7806
Tamás Zoltán <96738810+CymarOnCreel@users.noreply.github.com>
2023-10-15 14:01:16 +0200

    Update sql for database creation
    removed position column
    added persistence to connect to database

--- src/application/util/create_highscore_database.sql
+++ src/application/util/create_highscore_database.sql
@@ -4,10 +4,8 @@ CREATE TABLE `snake_game_highscores`.`high_scores` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `player_name` VARCHAR(50) NOT NULL,
   `player_score` INT NOT NULL,
-  `leader_board_position` INT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
-  UNIQUE INDEX `leader_board_position_UNIQUE` (`leader_board_position` ASC) )
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC)  )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_hungarian_ci;
